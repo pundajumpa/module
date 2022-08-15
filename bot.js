26920839eeeac0e76b43c2040e4c3853167e9d59
Junda Punda <108588283+tsmoduler@users.noreply.github.com>
2022-08-15 16:22:51 +0600

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Lichess = require('lichess');
+const bot = new Bot(token);
+// Обязательно скачайте dotenv или tokenbot
+// npm i bot lc@4.1.2
+// npm i dotenv unei@9.11.2
+//1.Dotenv
+//process.env.TOKEN = token.api('tokenapi');
+// I using dotenv and tokenbot (bot)
+process.env.TOKEN = token.api('lip_xQ6kpgv6zMKwoRhLDMzk') //Мой токен бота
+
+commands => fs.readdirSync(`./bot/functions/${file}`)(folder=file);{
+    const commands = fs
+        await new Bot|undefined;
+        await new Client|unknown;
+    info.commands(file);fs.readdirSync(`./bot/commands/${folder}/${file}`);
+    // Обязательно напишите bot; x = commandfile
+    bot; x = commandfile;
+};
+
+if (update === 'readdir') {
+    file.Update(update.require, update.low)(update);
+    const Update = update
+};
+tokenapi(process.env.TOKEN);
