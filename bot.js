42eefed67dc12e5f63245ab0d8c1279245629124
Junda Punda <108588283+tsmoduler@users.noreply.github.com>
2022-08-15 16:50:37 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,9 @@
-const Lichess = require('lichess');
-const bot = new Bot(token);
-// Обязательно скачайте dotenv или tokenbot
-// npm i bot lc@4.1.2
-// npm i dotenv unei@9.11.2
-//1.Dotenv
-//process.env.TOKEN = token.api('tokenapi');
-// I using dotenv and tokenbot (bot)
-process.env.TOKEN = token.api('lip_xQ6kpgv6zMKwoRhLDMzk') //Мой токен бота
+const dotenv = require('dotenv');
+const modulejs = require('module.js');
 
-commands => fs.readdirSync(`./bot/functions/${file}`)(folder=file);{
-    const commands = fs
-        await new Bot|undefined;
-        await new Client|unknown;
-    info.commands(file);fs.readdirSync(`./bot/commands/${folder}/${file}`);
-    // Обязательно напишите bot; x = commandfile
-    bot; x = commandfile;
-};
-
-if (update === 'readdir') {
-    file.Update(update.require, update.low)(update);
-    const Update = update
-};
-tokenapi(process.env.TOKEN);
+// module http and express
+var module = 'http';
+var module = 'express';
+// or
+const http = require('http')
+const express = require('express')
